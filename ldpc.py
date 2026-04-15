c76ab0d42144fb0c34675d9637961f12e597d8fb
Anurag Kashyap <anurakas@amazon.com>
2026-04-15 02:29:16 -0400

    added LDPC.detect

--- ldpc.py
+++ ldpc.py
@@ -37,14 +37,66 @@ def generate_PG(n: int, t: int, r: int, g: int, seed: int | None = None):
     assert not np.any(P @ G), "PG != 0"
     return P, G
 
+def sample_t_sparse_numpy(n: int, t: int) -> galois.FieldArray:
+    if t > n or t < 0:
+        raise ValueError("t must be between 0 and n")
+    vector = np.zeros(n, dtype=np.int8)
+    indices = np.random.choice(n, size=t, replace=False)
+    vector[indices] = 1
+    return GF2(vector)
+
+def bernoulli_noise(n: int, eta: float):
+    t =int(eta*n)
+    return sample_t_sparse_numpy(n, t)
+
+def add_error(vec: galois.FieldArray, eta: float):
+    n = vec.shape[0]
+    return vec + bernoulli_noise(n, eta)
+
+def weight(P, vec: galois.FieldArray):
+    return int(np.array(P@vec, dtype=int).sum())
+
+def detect(P, vec: galois.FieldArray):
+    wt = weight(P, vec)
+    r = P.shape[0]
+    threshold = (0.5 - (1/(r**4)))*r
+    return wt < threshold
+
+
+#def detect_rate()
+
+class LDPC_PRC:
+    def __init__(self, n):
+        self.n = n
+        self.eta = 0.4
+        self.t = int(np.log2(n))          # Θ(log n)
+        self.g = int(np.log2(n) ** 2)     # Ω(log² n)
+        self.r = self.n - self.g                    # or any r ≤ 0.99n
+        print(f"n is {self.n}")
+        print(f"g is {self.g}")
+        print(f"t is {self.t}")
+        print(f"r is {self.r}")
+        P, G = generate_PG(n, self.t, self.r, self.g, seed=0)
+
+        
 
 if __name__ == "__main__":
     n = 100
+    eta = 0.4
     t = int(np.log2(n))          # Θ(log n)
     g = int(np.log2(n) ** 2)     # Ω(log² n)
     r = n - g                    # or any r ≤ 0.99n
-
+    print(f"n is {n}")
+    print(f"g is {g}")
+    print(f"t is {t}")
+    print(f"r is {r}")
+    
     P, G = generate_PG(n, t, r, g, seed=0)
     print(f"P: {P.shape}, row weights: {np.asarray(P).sum(axis=1)[:5]}... (all == {t})")
     print(f"G: {G.shape}")
     print(f"PG = 0 ? {not np.any(P @ G)}")
+
+    s = GF2(np.random.binomial(1, 0.5, g))
+    x = add_error(G@s, 0.2)
+    print(weight(P, x))
+    print(detect(P,x))
